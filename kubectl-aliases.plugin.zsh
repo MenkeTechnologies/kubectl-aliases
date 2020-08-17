98cae747bb081bfd570b7b27414b8c749b70a491
MenkeTechnologies <jmenke@wccnet.edu>
2020-08-16 23:48:17 -0400

    add omz plug file

---
+++ kubectl-aliases.plugin.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Sun Aug 16 23:35:03 EDT 2020
+##### Purpose: zsh script to source more kubectl aliases
+##### Notes:
+#}}}***********************************************************
+
+# According to the standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+ZERO="${(%):-%N}" # this gives immunity to functionargzero being unset
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+local aliasesFile=${0:h}/.kubectl_aliases
+
+if [[ -f $aliasesFile ]]; then
+    source $aliasesFile
+else
+    echo "ERROR: $aliasesFile does not exist" >&2
+fi
